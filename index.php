095a78d1a34b835e43a8b5cf98e5dbd097914b78
Vladimir Romanov <vladromweb@gmail.com>
2015-10-11 17:39:52 -0400

    Customer & Club Features
    Added descriptions to features and corrected Bootstrap alignment.

--- index.php
+++ index.php
@@ -64,24 +64,74 @@
                     <h3 class="section-subheading text-muted">Just a small overview of some of our features.</h3>
                 </div>
             </div>
+            
             <div class="row text-center">
                 <div class="col-md-6">
-                    <span class="fa-stack fa-4x">
-                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fa fa-users fa-stack-1x fa-inverse"></i>
-                    </span>
-                    <h4 class="service-heading">Nightlife & Party People</h4>
-                    <p class="text-muted">Our goal is to make your nightlife experience flawless and rewarding. We are looking to facilitate the booking process as well as reward customers with VIP perks and experiences.</p>
+                	<span class="fa-stack fa-4x">
+    	                <i class="fa fa-circle fa-stack-2x text-primary"></i>
+        	            <i class="fa fa-users fa-stack-1x fa-inverse"></i>
+            	    </span>
+            	    <h2 class="service-heading">Nightlife & Party People</h4>
+                	<div class="row text-center">
+                		<div class="col-sm-6">
+                			<h4 class="service-heading">Simplified Booking</h4>
+                    		<p class="text-muted">The ability to make the reservation process as simple as possible is one of our main priorities. We want customers to be able to book venues with a click of a button.</p>
+               			</div>
+               	
+                		<div class="col-sm-6">
+                			<h4 class="service-heading">Split-The-Bill</h4>
+                    		<p class="text-muted">You have a large group of friends and don't want to deal with the hustle of collecting money from them? No problem, our split-the-bill feature will allow you to send personalized bills to each member of your party.</p>
+                		</div>
+                	</div>
+                	
+                	<div class="row text-center">
+                		<div class="col-sm-6">
+                			<h4 class="service-heading">Reward System</h4>
+                    		<p class="text-muted">We value our customers and want you to keep coming back. Our platform incorporates a rewards system which will make your experience enjoyable time after time.</p>
+               			</div>
+               	
+                		<div class="col-sm-6">
+                			<h4 class="service-heading">VIP Experience</h4>
+                    		<p class="text-muted">Our customers will enjoy an exceptional VIP experience without the usual challenges of making it happen.</p>
+                		</div>
+                	</div>
+                	
                 </div>
+                
                 <div class="col-md-6">
-                    <span class="fa-stack fa-4x">
-                        <i class="fa fa-circle fa-stack-2x text-primary"></i>
-                        <i class="fa fa-building-o fa-stack-1x fa-inverse"></i>
-                    </span>
-                    <h4 class="service-heading">Nightclubs and Venues</h4>
-                    <p class="text-muted">Our goal is to bring you new customers, facilitate the booking process and increase repeat sales!</p>
+            	    <span class="fa-stack fa-4x">
+    	                <i class="fa fa-circle fa-stack-2x text-primary"></i>
+        	            <i class="fa fa-building-o fa-stack-1x fa-inverse"></i>
+            	    </span>
+                	<h2 class="service-heading">Nightclubs and Venues</h4>
+                	<div class="row text-center">
+                		<div class="col-sm-6">
+                			<h4 class="service-heading">Reservation Management</h4>
+                    		<p class="text-muted">Our platform makes it easy for clubs to manage outstanding reservations and requests. You will be notified as a new reservation comes in, will be given the option to accept it and will receive follow-ups as needed.</p>
+               			</div>
+               	
+                		<div class="col-sm-6">
+                			<h4 class="service-heading">Personel Management</h4>
+                    		<p class="text-muted">We will give you the option to have several managers which would oversee the reservation process. You can give and revoke their administrative rights at any time through the platform.</p>
+                		</div>
+                	</div>
+                	
+                	<div class="row text-center">
+                		<div class="col-sm-6">
+                			<h4 class="service-heading">Promotional Features</h4>
+                    		<p class="text-muted">We want to give you the opportunity to reach your most valuable customers. Our mailing list will keep track of the users and allow you to get in touch when needed.</p>
+               			</div>
+               	
+                		<div class="col-sm-6">
+                			<h4 class="service-heading">User Analytics</h4>
+                    		<p class="text-muted">Knowing your customers is extremely valuable. Our platform will provide you with valuable metrics about the users and allow you to tailor services specifically for them.</p>
+                		</div>
+                	</div>
+                	
+                	
                 </div>
             </div>
+            
             <div class="row text-center">
             <a href="#contact" class="page-scroll btn btn-xl">Mailing List</a>
             </div>
